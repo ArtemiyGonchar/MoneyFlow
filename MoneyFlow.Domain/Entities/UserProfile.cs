9a50e962ce9bffc1c2d01e730a0fde72a2d8292d
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-27 13:21:35 +0300

    feat: Add BaseEntity, UserProfile entities ✨

---
+++ MoneyFlow.Domain/Entities/UserProfile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Domain.Entities
+{
+    public class UserProfile : BaseEntity
+    {
+        private UserProfile() { }
+
+        public UserProfile(string name)
+        {
+            Name = name;
+            CreatedAt = DateTime.UtcNow;
+            Categories = new List<Category>();
+            Transactions = new List<Transaction>();
+        }
+
+        public string Name { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public virtual ICollection<Category> Categories { get; private set; }
+        public virtual ICollection<Transaction> Transactions { get; private set; }
+
+        public void Rename(string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                throw new ArgumentNullException("Name cannot be empty.", nameof(newName));
+            }
+
+            Name = newName;
+        }
+    }
+}
