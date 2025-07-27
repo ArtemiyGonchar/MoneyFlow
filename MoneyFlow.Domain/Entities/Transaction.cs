6c5c5a630e47d230e6e69a8f74eb9798786ce68a
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-27 14:09:10 +0300

    feat: Added Transaction entity and Money ValueObject ✨

---
+++ MoneyFlow.Domain/Entities/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using MoneyFlow.Domain.Entities.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Domain.Entities
+{
+    public class Transaction : BaseEntity
+    {
+        private Transaction() { }
+
+        public Transaction(string title, string description, Guid userProfileId, Money amount)
+        {
+            SetTitle(title);
+            SetDescription(description);
+            UserProfileId = userProfileId;
+            Amount = amount;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+
+        public Guid UserProfileId { get; private set; }
+        public virtual UserProfile UserProfile { get; private set; }
+
+        public Money Amount { get; private set; }
+
+        public void SetTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title)) throw new ArgumentNullException(nameof(title));
+            Title = title;
+        }
+
+        public void SetDescription(string description)
+        {
+            if (string.IsNullOrWhiteSpace(description)) throw new ArgumentNullException(nameof(description));
+            Description = description;
+        }
+    }
+}
---
+++ MoneyFlow.Domain/Entities/ValueObjects/Money.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Domain.Entities.ValueObjects
+{
+    //[Owned]
+    public class Money
+    {
+        public decimal Amount { get; private set; }
+
+        private Money() { }
+        public Money(decimal amount)
+        {
+            if (amount < 0) throw new ArgumentOutOfRangeException(nameof(amount));
+            Amount = amount;
+        }
+    }
+}
