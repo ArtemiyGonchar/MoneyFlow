01a8b5c2fc4b010d78598a260bc9a294b45b32c4
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-27 13:43:22 +0300

    feat: Added Category entity ✨

---
+++ MoneyFlow.Domain/Entities/Category.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        private Category() { }
+
+        public Category(string title, Guid userProfileId)
+        {
+            SetTitle(title);
+            UserProfileId = userProfileId;
+        }
+
+        public string Title { get; private set; }
+        
+        public Guid UserProfileId { get; private set; }
+        public virtual UserProfile UserProfile { get; private set; }
+        
+        public void Rename(string newTitle)
+        {
+            SetTitle(newTitle);
+        }
+
+        private void SetTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("Title cannot be null or whitespace.", nameof(title));
+            }
+            
+            Title = title;
+        }
+    }
+}
