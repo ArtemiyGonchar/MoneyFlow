f06e37470060fe84408b93ebe4f1f7875eda9e12
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-29 12:03:05 +0300

    feat: Added empty repos for entities ✨

---
+++ MoneyFlow.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using MoneyFlow.Domain.Entities;
+using MoneyFlow.Domain.Interfaces.Repositories;
+using MoneyFlow.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly MoneyFlowDbContext _ctx;
+
+        public CategoryRepository(MoneyFlowDbContext ctx) : base(ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
---
+++ MoneyFlow.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using MoneyFlow.Domain.Entities;
+using MoneyFlow.Domain.Interfaces.Repositories;
+using MoneyFlow.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Infrastructure.Repositories
+{
+    public class UserRepository : Repository<UserProfile>, IUserProfileRepository
+    {
+        private readonly MoneyFlowDbContext _ctx;
+
+        public UserRepository(MoneyFlowDbContext ctx) : base(ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
