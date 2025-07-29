c0f32f3af113964dc6c8adf631dbc8de9345a790
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-29 15:11:11 +0300

    feat: Added Transaction configuration fo EF ✨

---
+++ MoneyFlow.Infrastructure/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Infrastructure.Configurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            //Configure Title property
+            builder.Property(t => t.Title)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            //Configure Description property
+            builder.Property(t => t.Description)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            //Configure value object Money
+            builder.OwnsOne(t => t.Amount, money =>
+            {
+                money.Property(m => m.Amount)
+                .IsRequired();
+            });
+        }
+    }
+}
