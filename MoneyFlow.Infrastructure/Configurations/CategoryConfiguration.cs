5e8df3c61ba44272ec3e96cf3e034f690d03ef4c
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-29 15:14:00 +0300

    feat: Added Category configuration fo EF ✨

---
+++ MoneyFlow.Infrastructure/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Infrastructure.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //Configure Title property
+            builder.Property(c => c.Title)
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
