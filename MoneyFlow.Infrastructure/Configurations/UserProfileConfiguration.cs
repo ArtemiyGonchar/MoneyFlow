34785b3e360dc047dceb3afe9551d72a92447274
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-07-29 14:34:09 +0300

    feat: Added UserProfile configuration for EF ✨

---
+++ MoneyFlow.Infrastructure/Configurations/UserProfileConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyFlow.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Infrastructure.Configurations
+{
+    public class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile>
+    {
+        //Configuring UserProfile entity for db
+        public void Configure(EntityTypeBuilder<UserProfile> builder)
+        {
+            //Name property
+            builder.Property(u => u.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            //CreatedAt property
+            builder.Property(u => u.CreatedAt)
+                .IsRequired();
+
+            //Configuring One to Many property with Category
+            builder.HasMany(u => u.Categories)
+                .WithOne(c => c.UserProfile)
+                .HasForeignKey(c => c.UserProfileId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //Configuring One to Many property with Transaction
+            builder.HasMany(u => u.Transactions)
+                .WithOne(t => t.UserProfile)
+                .HasForeignKey(t => t.UserProfileId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
