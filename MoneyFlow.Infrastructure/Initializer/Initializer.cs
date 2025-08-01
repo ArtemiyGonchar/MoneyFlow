e706f97f8229184c04741b17a4c490ce6c403a7b
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-08-01 13:41:21 +0300

    feat: Added db initializer to infrastructure ✨

---
+++ MoneyFlow.Infrastructure/Initializer/Initializer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyFlow.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Infrastructure.Initializer
+{
+    public class Initializer
+    {
+        private readonly MoneyFlowDbContext _context;
+
+        public Initializer(MoneyFlowDbContext context) { _context = context; }
+
+        public async Task InitializeDb(MoneyFlowDbContext ctx)
+        {
+            await ctx.Database.MigrateAsync();
+        }
+    }
+}
