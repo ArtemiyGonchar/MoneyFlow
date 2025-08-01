a79d72266bf9a276f9fdc6e4a0a7ba7d7acb8081
ArtemiyGonchar <artmiygonchar@gmail.com>
2025-08-01 14:04:59 +0300

    feat: Added infrastructure injection ✨

---
+++ MoneyFlow.Infrastructure/DI/InfrastructureInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoneyFlow.Domain.Interfaces.Repositories;
+using MoneyFlow.Infrastructure.Context;
+using MoneyFlow.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyFlow.Infrastructure.DI
+{
+    public static class InfrastructureInjection
+    {
+        public static void RegisterInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MoneyFlowDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DbConnectionString"));
+            });
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<IUserProfileRepository, IUserProfileRepository>();
+        }
+    }
+}
